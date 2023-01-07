b0ee70bea20b189cedf2facab41e7cad85e13b6e
erickems <erick.sena@ccc.ufcg.edu.br>
2023-01-07 14:12:32 -0300

    Prepara para deploy

--- expressConfig.js
+++ expressConfig.js
@@ -1,12 +1,18 @@
 require('./config/mongo.service.js')
 const cors = require('cors')
 const express = require('express')
+const cliente = require('./model/cliente/Cliente')
+const estabelecimento = require('./model/estabelecimento/Estabelecimento')
+const evento = require('./model/evento/Evento')
 const app = express()
 
 app.use(cors())
+app.use("/model/cliente/Cliente", cliente)
+app.use("/model/estabelecimento/Estabelecimento", estabelecimento)
+app.use("/model/evento/Evento", evento)
 app.use(
     express.urlencoded({extended:true}),
-    express.json(),
+    express.json()
 )
 
 module.exports = app
