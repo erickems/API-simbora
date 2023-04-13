961b28924bef7fcfd0500524717d013ffab780a1
erickems <erick.sena@ccc.ufcg.edu.br>
2023-04-13 18:37:03 -0300

    Adiciona chave primária em nome de cliente

--- model/cliente/Cliente.js
+++ model/cliente/Cliente.js
@@ -3,7 +3,7 @@ const Evento = require('./../evento/Evento')
 const mongoose = restful.mongoose
 
 const clienteSchema = new mongoose.Schema({
-  nome: { type: String, required: true },
+  nome: { type: String, required: true, unique: true },
   email: { type: String, required: true },
   senha: { type: String, required: true },
   evento_interesse: {type: [String]}
